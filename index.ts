dada182ad5ffafd86af8e7907d9db2255b18b5c5
lacomaco <lacomaco122@gmail.com>
2021-08-27 00:21:27 +0900

    3 files modified

--- index.ts
+++ index.ts
@@ -1,26 +1,57 @@
 import './style.css';
 
-import { fromEvent, map, from, debounceTime, mergeMap } from 'rxjs';
-import { catchError, distinctUntilChanged, filter } from 'rxjs/operators';
+import {
+  fromEvent,
+  map,
+  from,
+  debounceTime,
+  mergeMap,
+  tap,
+  partition
+} from 'rxjs';
+import {
+  catchError,
+  distinctUntilChanged,
+  filter,
+  finalize,
+  retry,
+  switchMap
+} from 'rxjs/operators';
+
 const keyup$ = fromEvent(document.getElementById('search'), 'keyup').pipe(
   debounceTime(1000),
   map((event: KeyboardEvent) => (event.target as HTMLInputElement).value),
-  distinctUntilChanged(),
-  filter(query => query.trim().length > 0),
-  mergeMap(query =>
+  distinctUntilChanged()
+);
+
+// let [user$, reset$] = keyup$.pipe(partition(() => true));
+let [user$, reset$] = partition(keyup$, value => {
+  return value.trim().length > 0;
+});
+
+user$ = user$.pipe(
+  tap(showLoading),
+  switchMap(query =>
     from(
       fetch(`https://api.github.com/search/users?q=${query}`).then(res =>
         res.json()
       )
     )
-  )
+  ),
+  tap(hideLoading),
+  retry(2),
+  finalize(hideLoading)
 );
 
-keyup$.subscribe(value => {
+reset$ = reset$.pipe(tap(v => ($layer.innerHTML = '')));
+
+const $layer = document.querySelector('#suggestLayer');
+
+user$.subscribe(value => {
   drawLayer(value.items);
 });
 
-const $layer = document.querySelector('#suggestLayer');
+reset$.subscribe();
 
 function drawLayer(items) {
   $layer.innerHTML = items
@@ -33,4 +64,14 @@ function drawLayer(items) {
     </li>`;
     })
     .join('');
+}
+
+const $loading = document.getElementById('loading');
+
+function showLoading() {
+  $loading.style.display = 'block';
+}
+
+function hideLoading() {
+  $loading.style.display = 'none';
 }
