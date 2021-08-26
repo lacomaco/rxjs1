909565349b37a4336a72a898e79be7bce135d446
lacomaco <lacomaco122@gmail.com>
2021-08-26 23:20:15 +0900

    3 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,36 @@
 import './style.css';
 
-import { of, map } from 'rxjs';
+import { fromEvent, map, from, debounceTime, mergeMap } from 'rxjs';
+import { catchError, distinctUntilChanged, filter } from 'rxjs/operators';
+const keyup$ = fromEvent(document.getElementById('search'), 'keyup').pipe(
+  debounceTime(1000),
+  map((event: KeyboardEvent) => (event.target as HTMLInputElement).value),
+  distinctUntilChanged(),
+  filter(query => query.trim().length > 0),
+  mergeMap(query =>
+    from(
+      fetch(`https://api.github.com/search/users?q=${query}`).then(res =>
+        res.json()
+      )
+    )
+  )
+);
 
-of('World')
-  .pipe(map(name => `Hello, ${name}!`))
-  .subscribe(console.log);
+keyup$.subscribe(value => {
+  drawLayer(value.items);
+});
 
-// Open the console in the bottom right to see results.
+const $layer = document.querySelector('#suggestLayer');
+
+function drawLayer(items) {
+  $layer.innerHTML = items
+    .map(user => {
+      return `<li class="user">
+      <img src="${user.avatar_url}" width="50px" height="50px"/>
+      <p>
+        <a href="${user.html_url}" target="_blank">${user.login}</a>
+      </p>
+    </li>`;
+    })
+    .join('');
+}
